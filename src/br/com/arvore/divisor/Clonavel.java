e81ebe9688a6fdd0182d35a4dd3cdf74d142b94b
Florêncio Vieira Filho <florenciovieira@gmail.com>
2018-06-05 19:59:31 -0300

    Atualização

---
+++ src/br/com/arvore/divisor/Clonavel.java
@@ -0,0 +1,7 @@
+package br.com.arvore.divisor;
+
+public interface Clonavel {
+
+	public Clonavel clonar();
+
+}
--- src/br/com/arvore/divisor/Divisor.java
+++ src/br/com/arvore/divisor/Divisor.java
@@ -12,16 +12,26 @@ public class Divisor extends SplitPane implements DivisorListener {
 	public static final byte ABAIXO = 3;
 	public static final byte ACIMA = 4;
 
-	public void clonarComponente(Component componente, byte local) {
+	public void clonarComponente(Clonavel componente, byte local) {
 		if (componente == null) {
 			return;
 		}
 
+		if (componente != leftComponent && componente != rightComponent) {
+			throw new IllegalStateException();
+		}
+
+		Clonavel clone = componente.clonar();
+		Divisor horizontal = null;
+		Divisor vertical = null;
+
 		if (local == ESQUERDO) {
 			if (componente == leftComponent) {
-
-			} else if (componente == leftComponent) {
-
+				horizontal = criarDivisorHorizontal((Component) clone, leftComponent);
+				setLeftComponent(horizontal);
+			} else if (componente == rightComponent) {
+				horizontal = criarDivisorHorizontal((Component) clone, leftComponent);
+				setLeftComponent(horizontal);
 			}
 		} else if (local == DIREITO) {
 			if (componente == leftComponent) {
@@ -42,6 +52,24 @@ public class Divisor extends SplitPane implements DivisorListener {
 
 			}
 		}
+	}
+
+	private Divisor criarDivisorVertical(Component acima, Component abaixo) {
+		return criarDivisor(VERTICAL_SPLIT, acima, abaixo);
+	}
+
+	private Divisor criarDivisorHorizontal(Component esquerdo, Component direito) {
+		return criarDivisor(HORIZONTAL_SPLIT, esquerdo, direito);
+	}
+
+	private Divisor criarDivisor(int orientacao, Component c1, Component c2) {
+		Divisor divisor = new Divisor();
+
+		divisor.setOrientation(orientacao);
+		divisor.setRightComponent(c1);
+		divisor.setLeftComponent(c2);
+
+		return divisor;
 	}
 
 	public void excluirComponente(Component componente) {
