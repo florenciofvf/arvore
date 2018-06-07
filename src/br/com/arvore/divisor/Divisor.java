aa2dd5a66caf206cac63c3a9da7ba510cd6c26c7
Florêncio Vieira Filho <florenciovieira@gmail.com>
2018-06-07 17:53:37 -0300

    Atualização

--- src/br/com/arvore/divisor/Divisor.java
+++ src/br/com/arvore/divisor/Divisor.java
@@ -8,6 +8,7 @@ public class Divisor extends SplitPane implements DivisorListener {
 	private static final long serialVersionUID = 1L;
 	private DivisorListener divisorListener;
 	public static final byte ESQUERDO = 1;
+	private MementoDivisor mementoDivisor;
 	public static final byte DIREITO = 2;
 	public static final byte ABAIXO = 3;
 	public static final byte ACIMA = 4;
@@ -19,6 +20,7 @@ public class Divisor extends SplitPane implements DivisorListener {
 	public void clonarLeft(byte disposicao) {
 		Component clone = DivisorUtil.clonarLeft(this);
 		Component left = leftComponent;
+		criarMementoDivisor();
 		remove(leftComponent);
 
 		if (disposicao == ESQUERDO) {
@@ -31,11 +33,14 @@ public class Divisor extends SplitPane implements DivisorListener {
 		} else if (disposicao == ACIMA) {
 			DivisorUtil.novoVerticalLeft(this, left, clone);
 		}
+
+		restaurarLocalizacaoDivisor();
 	}
 
 	public void clonarRight(byte disposicao) {
 		Component clone = DivisorUtil.clonarRight(this);
 		Component right = rightComponent;
+		criarMementoDivisor();
 		remove(rightComponent);
 
 		if (disposicao == ESQUERDO) {
@@ -48,6 +53,8 @@ public class Divisor extends SplitPane implements DivisorListener {
 		} else if (disposicao == ACIMA) {
 			DivisorUtil.novoVerticalRight(this, right, clone);
 		}
+
+		restaurarLocalizacaoDivisor();
 	}
 
 	public void excluirLeft() {
@@ -72,14 +79,20 @@ public class Divisor extends SplitPane implements DivisorListener {
 		Component componente = divisor.getRightComponent();
 
 		if (divisor == leftComponent) {
+
 			DivisorUtil.configDivisor(this, componente);
 			DivisorUtil.setLeftTrue(componente);
+			criarMementoDivisor();
 			setLeftComponent(componente);
+			restaurarLocalizacaoDivisor();
 
 		} else if (divisor == rightComponent) {
+
 			DivisorUtil.configDivisor(this, componente);
 			DivisorUtil.setLeftFalse(componente);
+			criarMementoDivisor();
 			setRightComponent(componente);
+			restaurarLocalizacaoDivisor();
 
 		} else {
 			throw new IllegalStateException();
@@ -92,17 +105,45 @@ public class Divisor extends SplitPane implements DivisorListener {
 		Component componente = divisor.getLeftComponent();
 
 		if (divisor == leftComponent) {
+
 			DivisorUtil.configDivisor(this, componente);
 			DivisorUtil.setLeftTrue(componente);
+			criarMementoDivisor();
 			setLeftComponent(componente);
+			restaurarLocalizacaoDivisor();
 
 		} else if (divisor == rightComponent) {
+
 			DivisorUtil.configDivisor(this, componente);
 			DivisorUtil.setLeftFalse(componente);
+			criarMementoDivisor();
 			setRightComponent(componente);
+			restaurarLocalizacaoDivisor();
 
 		} else {
 			throw new IllegalStateException();
+		}
+	}
+
+	private void criarMementoDivisor() {
+		mementoDivisor = new MementoDivisor(this);
+	}
+
+	private void restaurarLocalizacaoDivisor() {
+		if (mementoDivisor != null) {
+			mementoDivisor.restaurar();
+		}
+	}
+
+	private class MementoDivisor {
+		final int localizacao;
+
+		MementoDivisor(Divisor divisor) {
+			localizacao = divisor.getDividerLocation();
+		}
+
+		void restaurar() {
+			setDividerLocation(localizacao);
 		}
 	}
 }
