5aa00ead6e8d6b6548e3c39ce4d0024ee6c266ce
Florêncio Vieira Filho <florenciovieira@gmail.com>
2018-07-30 20:40:37 -0300

    Atualização

--- src/br/com/arvore/rotulo/RotuloListener.java
+++
@@ -1,7 +0,0 @@
-package br.com.arvore.rotulo;
-
-public interface RotuloListener {
-	public void exibirPopup(Rotulo rotulo, int x, int y);
-
-	public void selecionarTitulo(Rotulo rotulo);
-}
--- src/br/com/arvore/tabela/TabelaListener.java
+++
@@ -1,7 +0,0 @@
-package br.com.arvore.tabela;
-
-import javax.swing.table.TableColumn;
-
-public interface TabelaListener {
-	public void ordenarColuna(TableColumn tableColumn, boolean descendente, int modelColuna);
-}
